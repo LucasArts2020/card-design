57b15cb439700704af972fb4252b981d706cd7f9
LucasArts2020 <lucasarts2020@gmail.com>
2025-03-28 15:28:50 -0300

    new file:   images/bg-card-front.png 	new file:   images/bg-main-desktop.png 	new file:   images/bg-main-mobile.png 	new file:   images/card-logo.svg 	new file:   images/favicon-32x32.png 	new file:   images/icon-complete.svg 	new file:   index.html 	new file:   style-guide.md
    Commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const cardNumber = document.getElementById("escreverNumero");
+const numberCartao = document.getElementById("numero");
+
+cardNumber.addEventListener("input", () => {
+  numberCartao.value = cardNumber.value;
+});
+
+const nomeInput = document.getElementById("nomeInput");
+const nome = document.getElementById("nome");
+
+nomeInput.addEventListener("input", () => {
+  nome.value = nomeInput.value;
+});
+
+const MM = document.getElementById("MM");
+const YY = document.getElementById("YY");
+const data = document.getElementById("data");
+
+function dataCartao() {
+  data.value = MM.value + "/" + YY.value;
+}
+
+MM.addEventListener("input", dataCartao);
+YY.addEventListener("input", dataCartao);
+
+const CVC = document.getElementById("CVC");
+const cartaoCvc = document.getElementById("cartaoCvc");
+
+CVC.addEventListener("input", () => {
+  cartaoCvc.value = CVC.value;
+});
